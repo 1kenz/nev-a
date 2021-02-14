22ba58f1eecf6fe37a77b47192d4044ff4a33247
xkendx <xkendx@users.noreply.github.com>
2021-02-14 13:56:26 +0100

    feat: functions

--- recap.js
+++ recap.js
@@ -1,2 +1,22 @@
 // recap js
 
+// var name = "Ken"
+
+// var surname = "Den"
+
+// console.log(name + " " + surname);
+
+function createNumber(number = 49) {
+    return console.log(Math.ceil(Math.random() * number));
+}
+
+var number1 = createNumber();
+var number2 = createNumber();
+var number3 = createNumber();
+var number4 = createNumber();
+var number5 = createNumber();
+var number6 = createNumber();
+
+console.log(number1, number2, number3, number4, number5, number6);
+
+console.log();
