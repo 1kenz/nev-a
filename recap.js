5fdf9caac51ba0ca4af95ab293703bbf8c394f0a
xkendx <xkendx@users.noreply.github.com>
2021-02-14 14:40:56 +0100

    feat: conditionals

--- recap.js
+++ recap.js
@@ -6,17 +6,25 @@
 
 // console.log(name + " " + surname);
 
-function createNumber(number = 49) {
-    return console.log(Math.ceil(Math.random() * number));
-}
+// function createNumber(number = 49) {
+//     return console.log(Math.ceil(Math.random() * number));
+// }
 
-var number1 = createNumber();
-var number2 = createNumber();
-var number3 = createNumber();
-var number4 = createNumber();
-var number5 = createNumber();
-var number6 = createNumber();
+// var number1 = createNumber();
+// var number2 = createNumber();
+// var number3 = createNumber();
+// var number4 = createNumber();
+// var number5 = createNumber();
+// var number6 = createNumber();
 
-console.log(number1, number2, number3, number4, number5, number6);
+// console.log(number1, number2, number3, number4, number5, number6);
 
-console.log();
+// CONDITIONALS
+
+var note = 51;
+
+if (note >= 50) {
+    console.log('Geçtiniz');
+} else if (note >= 40) {
+    console.log('Bütünleme');
+} else console.log('Kaldınız');
